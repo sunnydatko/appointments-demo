6abf4a0fe412728e8bd469198999b460a84fb7f8
Sunny <sunnydatko@gmail.com>
2019-11-29 14:24:00 -0800

    added null checks and misc cleanup

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
